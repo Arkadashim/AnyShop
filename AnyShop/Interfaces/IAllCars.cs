5e14d16972f8df21183fe19380c49a59fad46a4d
Arkadashim <andrewsha.antonov@mail.ru>
2023-06-30 15:42:37 +0300

    add interfaces

---
+++ AnyShop/Interfaces/IAllCars.cs
@@ -0,0 +1,11 @@
+﻿using AnyShop.Models;
+
+namespace AnyShop.Interfaces
+{
+    public interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; }
+        IEnumerable<Car> getFavCars { get; set; }
+        Car getObjectCars(int carID);
+    }
+}
---
+++ AnyShop/Interfaces/ICarsCategory.cs
@@ -0,0 +1,9 @@
+﻿using AnyShop.Models;
+
+namespace AnyShop.Interfaces
+{
+    public interface ICarsCategory
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
