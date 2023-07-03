d87d9633ae36895c5f67e20f4219306e16b32702
Arkadashim <andrewsha.antonov@mail.ru>
2023-07-03 12:32:24 +0300

    create new controller

---
+++ AnyShop/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿using AnyShop.Interfaces;
+using AnyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnyShop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iAllCars;
+            _allCategories = iCarsCat;
+        }
+
+        public ViewResult List()
+        {
+            //var cars = _allCars.Cars;
+            ViewBag.Title = "Страница с автомобилями";
+            CarsListViewModel obj = new CarsListViewModel();
+            obj.allCars = _allCars.Cars;
+            obj.currCategory = "Автомобили";
+
+            return View(obj);
+        }
+    }
+}
