f9de95ebd44d8bd20ee185bbbe17dff4a896db73
Arkadashim <andrewsha.antonov@mail.ru>
2023-07-03 12:33:13 +0300

    create a VM

---
+++ AnyShop/ViewModels/CarsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AnyShop.Models;
+
+namespace AnyShop.ViewModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> allCars { get; set; }
+        public string currCategory { get; set; }
+    }
+}
